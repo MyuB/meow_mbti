cf4a4eb8bed3e135fe2ee6483fff3cbbf785497e
MyuB <mjycoco0@naver.com>
2022-12-19 18:37:21 +0900

    Question Logic added

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { ProgressBar, Button } from "react-bootstrap";
 import { QuestionData } from "../assets/data/QuestionData";
+import { useNavigate } from "react-router-dom";
 
 const Wrapper = styled.div`
   height: 100vh;
@@ -23,20 +24,65 @@ const ButtonWrapper = styled.div`
 `;
 
 const Question = () => {
+  const [questionNo, setQuestionNo] = useState(0);
+  const [totalScore, setTotalScore] = useState([
+    { id: "EI", score: 0 },
+    { id: "SN", score: 0 },
+    { id: "TF", score: 0 },
+    { id: "JP", score: 0 },
+  ]);
+
+  const navigate = useNavigate();
+
+  const handleClickButton = (n, type) => {
+    const newScore = totalScore.map((s) =>
+      s.id === type ? { id: s.id, score: s.score + n } : s
+    );
+
+    setTotalScore(newScore);
+    // if (type === "EI") {
+    //   const addScore = totalScore[0].score + n;
+    //   const newObject = { id: "EI", score: addScore };
+    //   totalScore.splice(0, 1, newObject);
+    // } else if (type === "SN") {
+    //   const addScore = totalScore[1].score + n;
+    //   const newObject = { id: "EI", score: addScore };
+    //   totalScore.splice(1, 1, newObject);
+    // } else if (type === "TF") {
+    //   const addScore = totalScore[2].score + n;
+    //   const newObject = { id: "EI", score: addScore };
+    //   totalScore.splice(2, 1, newObject);
+    // } else {
+    //   const addScore = totalScore[3].score + n;
+    //   const newObject = { id: "EI", score: addScore };
+    //   totalScore.splice(3, 1, newObject);
+    // }
+    if (QuestionData.length !== questionNo + 1) setQuestionNo(questionNo + 1);
+    else {
+      navigate("/result");
+    }
+  };
+
+  console.log(totalScore);
+
   return (
     <Wrapper>
       <ProgressBar
         striped
         variant="danger"
-        now={80}
+        now={(questionNo / QuestionData.length) * 100}
         style={{ marginTop: "20px" }}
       />
-      <Title>{QuestionData[0].title}</Title>
+      <Title>{QuestionData[questionNo].title}</Title>
       <ButtonWrapper>
-        <Button style={{ width: "40%", minHeight: "200px", fontSize: "15pt" }}>
-          {QuestionData[0].answera}
+        <Button
+          onClick={() => handleClickButton(1, QuestionData[questionNo].type)}
+          style={{ width: "40%", minHeight: "200px", fontSize: "15pt" }}
+        >
+          {QuestionData[questionNo].answera}
         </Button>
         <Button
+          onClick={() => handleClickButton(0, QuestionData[questionNo].type)}
           style={{
             width: "40%",
             minHeight: "200px",
@@ -44,7 +90,7 @@ const Question = () => {
             marginLeft: "20px",
           }}
         >
-          {QuestionData[0].answerb}
+          {QuestionData[questionNo].answerb}
         </Button>
       </ButtonWrapper>
     </Wrapper>
